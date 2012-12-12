b3510fbd1a43029177812eff4c4c1f1fba4e2fc8
Salman Ahmad <salman@salmanahmad.com>
2012-12-12 14:29:15 -0500

    Adding the python dependencies and fixing the liblinear installation.

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 mkdir -p "$DIR/lib"
-cd "$DIR/lib"
+
+# Get python dependencies
+sudo easy_install pip
+sudo easy_install simplejson
+sudo pip install Flask
+sudo pip install -U pyyaml nltk
+
 
 # Get libsvm
+cd "$DIR/lib"
 wget "http://www.csie.ntu.edu.tw/~cjlin/cgi-bin/libsvm.cgi?+http://www.csie.ntu.edu.tw/~cjlin/libsvm+zip" -O "libsvm-3.14.zip"
 unzip "libsvm-3.14.zip"
 
@@ -15,7 +21,9 @@ rm "libsvm-3.14.zip"
 cd "libsvm"
 make
 
+
 # Get liblinear
+cd "$DIR/lib"
 wget "http://www.csie.ntu.edu.tw/~cjlin/cgi-bin/liblinear.cgi?+http://www.csie.ntu.edu.tw/~cjlin/liblinear+zip" -O "liblinear-1.92.zip"
 unzip "liblinear-1.92.zip"
 
@@ -24,3 +32,10 @@ rm "liblinear-1.92.zip"
 
 cd "liblinear"
 make
+
+
+# Get crfsuite
+
+
+
+
