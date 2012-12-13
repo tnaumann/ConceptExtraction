fd826025d36e14d4db453c4b391ee1361921f4f7
Salman Ahmad <salman@salmanahmad.com>
2012-12-12 22:51:11 -0500

    Fixing the statistics in evaluate

--- code/evaluate.py
+++ code/evaluate.py
@@ -36,26 +36,30 @@ def main():
 		print "%s %s" % (act.rjust(pad), "\t".join([str(confusion[act_v][pre_v]) for pre, pre_v in labels.items()]))
 	print
 	
+	
 	precision = []
 	recall = []
+	specificity = []
 	f1 = []
 	print "Analysis"
 	print " " * pad, "Precision\tRecall\tF1"
 	for lab, lab_v in labels.items():
 		tp = confusion[lab_v][lab_v]
-		fp = sum(confusion[v][lab_v] for k, v in labels.items())
-		fn = sum(confusion[lab_v][v] for k, v in labels.items())
+		fp = sum(confusion[v][lab_v] for k, v in labels.items() if v != lab_v)
+		fn = sum(confusion[lab_v][v] for k, v in labels.items() if v != lab_v)
 		tn = sum(confusion[v1][v2] for k1, v1 in labels.items() 
-			for k2, v2 in labels.items() if k1 != lab and k2 != lab)
+			for k2, v2 in labels.items() if v1 != lab_v and v2 != lab_v)
 		precision += [float(tp) / (tp + fp + 1e-100)]
 		recall += [float(tp) / (tp + fn + 1e-100)]
+		specificity += [float(tn) / (tn + fp + 1e-100)]
 		f1 += [float(2 * tp) / (2 * tp + fp + fn + 1e-100)]
 		print "%s %.4f\t%.4f\t%.4f" % (lab.rjust(pad), precision[-1], recall[-1], f1[-1])
 	print "--------"
 	precision = sum(precision) / len(precision)
 	recall = sum(recall) / len(recall)
+	specificity = sum(specificity) / len(specificity)
 	f1 = sum(f1) / len(f1)
-	print "Average: %.4f\t%.4f\t%.4f" % (precision, recall, f1)
+	print "Average: %.4f\t%.4f\t%.4f\t%.4f" % (precision, recall, specificity, f1)
 	
 if __name__ == '__main__':
 	main()
